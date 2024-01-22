94d478ce9253a4cfd4ee0169005f7c13cdd97eae
Dimas785 <dimasmanyu99@gmail.com>
2024-01-22 23:26:24 +0700

    penambahan html

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const path = require("path");
 
-const router = require('./routes')
+const router = require("./routes");
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(router)
+app.use(router);
 
-app.listen(8000, () =>
-  console.log('App listening at port http://localhost:8000'),
-)
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(8000, () => console.log("App listening at port http://localhost:8000"));
